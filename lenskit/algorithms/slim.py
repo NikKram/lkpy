cc66f969296a8591f2d7c8c88e94765f4027774d
Michael Ekstrand <md@ekstrandom.net>
2019-08-07 17:13:15 -0600

    more slim code

--- lenskit/algorithms/slim.py
+++ lenskit/algorithms/slim.py
@@ -7,7 +7,9 @@ import logging
 import numpy as np
 import pandas as pd
 
-from numba import njit
+from numba import njit, prange
+
+from ..matrix import CSR, sparse_ratings
 
 _log = logging.getLogger(__name__)
 
@@ -29,13 +31,12 @@ def _ind_add_sum_old(ys, iis, istart, iend, x):
 
 
 @njit
-def _cd_round(item, matrix, tmat, nbrs, weights, l2reg, l1reg):
+def _cd_round(item, tmat, nbrs, weights, l2reg, l1reg):
     """
     One round of solving an item's weights with coordinate descent.
 
     Args:
         item(int): the item we are scoring
-        matrix(CSR): the user-item rating matrix
         tmat(CSR): the item-user rating matrix
         nbrs(ndarray): the candidate neighbors (co-rated items)
         weights(ndarray): the current or initial coefficients for nbrs
@@ -46,7 +47,7 @@ def _cd_round(item, matrix, tmat, nbrs, weights, l2reg, l1reg):
         ndarray: the new coefficient weights
     """
     # compute residuals for this item for users rating neighbors
-    resid = np.zeros(matrix.nrows)
+    resid = np.zeros(tmat.ncols)
     # start by computing -pred
     for ji, j in enumerate(nbrs):
         s, e = tmat.row_extent(j)
@@ -60,7 +61,7 @@ def _cd_round(item, matrix, tmat, nbrs, weights, l2reg, l1reg):
         lb, ub = tmat.row_extent(j)
         # extract the gradient and take out current weights for update
         grad = _ind_add_sum_old(resid, tmat.colinds, lb, ub, weights[ji])
-        grad /= matrix.nrows
+        grad /= tmat.ncols
         # update weights
         if np.abs(grad) <= l1reg:
             weights[ji] = 0.0
@@ -74,3 +75,30 @@ def _cd_round(item, matrix, tmat, nbrs, weights, l2reg, l1reg):
         # update residuals with new weights
         if weights[ji] > 0:
             _ind_add(resid, tmat.colinds, lb, ub, -weights[ji])
+
+
+@njit
+def _find_nbrs(item, matrix, tmat):
+    counts = np.zeros(matrix.ncols, dtype='i4')
+    for u in tmat.row_cs(item):
+        counts[matrix.row_cs(u)] = 1
+    nbrs, = np.nonzero(counts)
+    return nbrs
+
+
+@njit(parallel=True)
+def _train_item_weights(matrix, tmat, l2reg, l1reg):
+    """
+    Train item weights for SLIM.
+
+    Args:
+        matrix(CSR): the
+    """
+    nitems = matrix.ncols
+
+    for i in prange(nitems):
+        nbrs = _find_nbrs(i, matrix, tmat)
+        if len(nbrs) == 0:
+            continue
+
+        weights = np.random.randn()
